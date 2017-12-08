2ee25fe6d317eb13216ba2cfbcee8e2ee58518cf
Tsvetkovv <11594423+Tsvetkovv@users.noreply.github.com>
2017-12-08 17:42:16 +0400

    added source code link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,6 +9,15 @@ class Footer extends React.Component {
         <div className={s.container}>
           <span className={s.text}>© 2014 - 2017 IBS Production </span>
           <span className={s.spacer}>·</span>
+          <a
+            className={s.text}
+            href="https://github.com/Tsvetkovv/videoportal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source code on GitHub
+          </a>
+          <span className={s.spacer}>·</span>
           <span className={s.text}>All Rights Reserved.</span>
         </div>
       </div>
